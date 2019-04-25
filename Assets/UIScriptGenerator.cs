59bdb82f3b61020159c55407c93c3d03d5ecdad0
Cheese Allergy Hamsters <49047211+shlifedev@users.noreply.github.com>
2019-04-25 22:34:27 +0900

    UIScriptGenerator
    UIScriptGenerator

---
+++ Assets/UIScriptGenerator.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIScriptGenerator
+{
+    public static string CompleteCode = null;
+
+    private static List<Transform> GetChilds(Transform target)
+    {
+        List < Transform >transformList = new List<Transform>();
+        for (int i = 0; i < target.transform.childCount; i++)
+        {
+            var child = target.transform.GetChild(i);
+            transformList.Add(child);
+        }
+        return transformList;
+    }
+    private static void DeepSearch(Transform target, List<Transform> makedList)
+    { 
+        List<Transform> childs = GetChilds(target);
+        if (childs.Count == 0)
+        {
+            return;
+        }
+        else
+        {
+
+            for (int i = 0; i < childs.Count; i++)
+            {
+                makedList.Add(childs[i]);
+                DeepSearch(childs[i], makedList);
+            }
+        }
+
+    }
+
+    private static List<List<Transform>> GetObjectChilds(Transform transform)
+    {
+        return null;
+    }
+
+    public static List<Transform> Search(Transform target)
+    {
+        var transformList = new List<Transform>();
+        DeepSearch(target, transformList); 
+        return transformList;
+    }
+
+
+    [MenuItem("GameObject/Run Generator", false, -1)]
+    public static void GenScript(MenuCommand command)
+    {
+        Transform selectedObj = UnityEditor.Selection.activeTransform;
+        if (selectedObj == null) return;
+        if (selectedObj.GetComponentInParent<Canvas>() == null) return;
+
+        var list = Search(selectedObj); 
+        CodeGenerator(list);
+    }
+
+
+    private static void CodeGenerator(List<Transform> transforms)
+    {
+
+        StringBuilder builder = new StringBuilder();
+        if (UIScriptGeneratorEditor.WriteRegion == true)
+            builder.AppendLine("#region " + UIScriptGeneratorEditor.Region +"\n");
+
+
+        for (int i = 0; i < transforms.Count; i++)
+        {
+            var compArr = transforms[i].GetComponents<MaskableGraphic>(); 
+            foreach (var component in compArr)
+            { 
+                string ret = null;
+                if (component is Image)
+                { 
+                    ret = Write(UIScriptGeneratorEditor.AttrStr + UIScriptGeneratorEditor.AplStr, component.GetType().Name, component.name); 
+                }
+                else if (component is Text)
+                {
+                    ret = Write(UIScriptGeneratorEditor.AttrStr + UIScriptGeneratorEditor.AplStr, component.GetType().Name, component.name); 
+                }
+                if(ret != null)
+                   builder.AppendLine(ret);
+            }
+        }
+
+        if (UIScriptGeneratorEditor.WriteRegion)
+            builder.AppendLine("\n#endregion");
+
+  CompleteCode = builder.ToString();
+        UIScriptGeneratorEditor.OpenWindow();
+
+    }
+
+     
+
+    private static string Write(string accessMidifier, string variableType, string variableName)
+    {
+        StringBuilder builder = new StringBuilder();
+        builder.Append(accessMidifier + " " );
+        builder.Append(variableType   + " ");
+        builder.Append(variableName   + ";");
+        return builder.ToString();
+    }
+}
---
+++ Assets/UIScriptGeneratorEditor.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEditor;
+using UnityEngine;
+
+public class UIScriptGeneratorEditor : UnityEditor.EditorWindow
+{
+
+    public enum AccessPermissionLevel { None, PUBLIC, PRIVATE};
+    public enum Attribute { None, SerializedField };
+    private static bool writeRegion = false;
+    private static string region = "SetRegionName";
+
+    private static AccessPermissionLevel apl;
+    private static Attribute attr;
+    private static UIScriptGeneratorEditor m_window = null;
+
+    public static AccessPermissionLevel Apl { get => apl; }
+    public static string AplStr
+    {
+        get
+        { 
+            if (apl == AccessPermissionLevel.None) return null;
+            if (apl == AccessPermissionLevel.PRIVATE) return "private";
+            if (apl == AccessPermissionLevel.PUBLIC) return "public";
+
+            return null;
+        }
+    }
+    public static Attribute Attr { get => attr;}
+    public static string AttrStr
+    {
+        get {
+            if (attr == Attribute.None) return null;
+            var ret = "[" + attr.ToString() + "] ";
+            return ret;
+        }
+    }
+
+    public static bool WriteRegion { get => writeRegion; set => writeRegion = value; }
+    public static string Region { get => region; set => region = value; }
+
+    [MenuItem("UIScriptGeneratorEditor/Code")]
+    public static void OpenWindow()
+    {
+        if (m_window == null)
+            m_window = EditorWindow.GetWindow(typeof(UIScriptGeneratorEditor)) as UIScriptGeneratorEditor;
+        m_window.titleContent = new GUIContent("QuickCode", "Make by.CheeseAllergyHamster ");
+        m_window.Show();
+
+    }
+
+    private void DrawSourceCodeWindow()
+    { 
+        GUILayout.Label("Generated Code :)");
+        UIScriptGenerator.CompleteCode = EditorGUILayout.TextArea(UIScriptGenerator.CompleteCode);
+
+        GUILayout.Label("Option");
+
+        Region = EditorGUILayout.TextField(Region);
+        WriteRegion = GUILayout.Toggle(WriteRegion, "Use Region");
+        apl  = (AccessPermissionLevel)EditorGUILayout.EnumPopup("AccessPermissionLevel", Apl);
+        attr = (Attribute)EditorGUILayout.EnumPopup("Attribute", Attr);
+
+    }
+    private void OnGUI()
+    {
+        DrawSourceCodeWindow();
+    }
+}
+     
+
+
